f7f31f21de8c92a6529993aa20f44f3505f6a8cf
Renan Campregher <renan.calanca@hotmail.com>
2018-12-16 23:55:24 -0200

    att

--- DatingApp.API/DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/DatingApp.API/Controllers/AuthController.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
 {
@@ -14,12 +19,13 @@ namespace DatingApp.API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-
         private readonly IAuthRepository _repository;
+        private readonly IConfiguration _config;
 
-        public AuthController(IAuthRepository repository)
+        public AuthController(IAuthRepository repository, IConfiguration config)
         {
             _repository = repository;
+            _config = config;
         }
 
         [HttpPost("register")]
@@ -46,12 +52,37 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            var userFromRepository = await _repository.Login(userForLoginDto.Username, userForLoginDto.Password);
+            var userFromRepository = await _repository.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepository == null)
                 return Unauthorized();
 
-            return BadRequest();
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepository.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepository.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token)
+            });
         }
 
 
