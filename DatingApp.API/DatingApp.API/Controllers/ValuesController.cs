5848519b952a2635ae2fa01e7dbeccea54fa1db0
Renan Campregher <renan.calanca@hotmail.com>
2018-12-17 15:44:56 -0200

    ATT

--- DatingApp.API/DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/DatingApp.API/Controllers/ValuesController.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
+    //Requires token for every action in this class
+    [Authorize]
     [Route("api/[controller]")]
     //Validation and routing
     [ApiController]
